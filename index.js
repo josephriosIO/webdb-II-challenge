13a6e4ea2a34712c2558ac9516983271ace91a40
josephriosio <blankworldwide@gmail.com>
2019-04-30 12:25:21 -0700

    created a dymanic port for index

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const helmet = require('helmet');
+const server = require("./server");
 
-const server = express();
+const port = process.env.PORT || 4000;
 
-server.use(express.json());
-server.use(helmet());
-
-// endpoints here
-
-const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
